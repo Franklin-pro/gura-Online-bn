2c7ffcf0aa2abf8111d809d45bb4e6a15c20bda5
Franklin-pro <franklinprogrammer@gmail.com>
2025-06-27 17:12:15 +0200

    hosting time

--- libs/stripe.js
+++ libs/stripe.js
@@ -1,7 +1,8 @@
 import Stripe from "stripe";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+apiVersion: '2025-05-28',
+});
